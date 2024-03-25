1f4deea1d89e4bf28e1dbbdf8b89919ff48aed1a
Tran Trung <38261549+lKulRo@users.noreply.github.com>
2024-03-25 23:13:06 +0100

    update ng

--- frontend/to-do-app/components/to-do-list/to-do-list-item-detail/to-do-list-item-detail.component.ts
+++ frontend/to-do-app/components/to-do-list/to-do-list-item-detail/to-do-list-item-detail.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { ActivatedRoute } from '@angular/router';
+import { ToDoListService } from '../../../services/to-do-list.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-to-do-list-item-detail',
   standalone: true,
-  imports: [],
+  imports: [MatIconModule, CommonModule],
   templateUrl: './to-do-list-item-detail.component.html',
-  styleUrl: './to-do-list-item-detail.component.css'
+  styleUrl: './to-do-list-item-detail.component.css',
 })
 export class ToDoListItemDetailComponent {
+  route: ActivatedRoute = inject(ActivatedRoute);
+  toDoListService = inject(ToDoListService);
 
+  id = Number(this.route.snapshot.params['id']);
+  item = this.toDoListService.getToDoListItemsDetailById(this.id);
 }
