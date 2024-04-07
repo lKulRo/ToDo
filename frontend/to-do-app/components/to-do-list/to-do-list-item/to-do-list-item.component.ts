958fff7e914cffc3346f66a6efca3dedcd7b289f
Tran Trung <38261549+lKulRo@users.noreply.github.com>
2024-04-07 18:08:27 +0200

    add updateTodos

--- frontend/to-do-app/components/to-do-list/to-do-list-item/to-do-list-item.component.ts
+++ frontend/to-do-app/components/to-do-list/to-do-list-item/to-do-list-item.component.ts
@@ -4,20 +4,50 @@ import { RouterModule } from '@angular/router';
 import { MatIconModule } from '@angular/material/icon';
 import { ToDoListService } from '../../../services/to-do-list.service';
 import { MatListModule } from '@angular/material/list';
+import { MatInputModule } from '@angular/material/input';
+import { FormGroup, FormControl, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @Component({
   selector: 'app-to-do-list-item',
   standalone: true,
-  imports: [RouterModule, MatIconModule, MatListModule],
+  imports: [
+    RouterModule,
+    MatIconModule,
+    MatListModule,
+    MatInputModule,
+    MatFormFieldModule,
+    ReactiveFormsModule,
+  ],
   templateUrl: './to-do-list-item.component.html',
-  styleUrl: './to-do-list-item.component.css'
+  styleUrl: './to-do-list-item.component.css',
 })
 export class ToDoListItemComponent {
   @Input() toDoListItem!: ToDoListItem;
   @Output() deleted = new EventEmitter();
   toDoListService: ToDoListService = inject(ToDoListService);
+  editOn = false;
+
+  editForm: FormGroup = new FormGroup({
+    edit: new FormControl(""),
+  });
   
-  deleteToDoList(){
-    this.toDoListService.deleteToDoLists(this.toDoListItem.id).subscribe({complete: () => this.deleted.emit()});
+  deleteToDoList() {
+    this.toDoListService
+      .deleteToDoLists(this.toDoListItem.id)
+      .subscribe({ complete: () => this.deleted.emit() });
+  }
+
+  edit() {
+    this.editOn = !this.editOn;
+  }
+  editDefaultValue(){
+    this.editForm.setValue({edit: this.toDoListItem.name})
+  }
+  updateToDoList(text: string){
+    this.toDoListService.updateToDoLists(text, this.toDoListItem.id).subscribe({complete: () => {
+      this.edit();
+      this.deleted.emit();
+    }})
   }
 }
